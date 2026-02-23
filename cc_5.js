28701080dc8d196de1a47f5543bacd8b0fe3ff25
Julio Cascoe <Juliocasco@usf.edu>
2026-02-22 20:00:37 -0500

    Step 2

---
+++ cc_5.js
@@ -0,0 +1,12 @@
+// Payroll Processing Toolkit
+
+//Step 2 - Array Employee Names
+const employees = [
+   {name: "Julio Casco", hourlyRate: 18.50, hoursWorked: 40},
+    {name: "Cesar Santoyo", hourlyRate: 22.00, hoursWorked: 35},
+    {name: "Maria Garcia", hourlyRate: 20.00, hoursWorked: 45},
+    {name: "Ana Lopez", hourlyRate: 19.00, hoursWorked: 30},
+    {name: "Tyler Smith", hourlyRate: 21.50, hoursWorked: 40}
+];
+
+//step 3 - Base Pay only up to 40 hours
